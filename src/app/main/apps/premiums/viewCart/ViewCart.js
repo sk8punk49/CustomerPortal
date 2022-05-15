0a1c00f6cb67c63f2deb70eb685798f735c30f1d
Ryan <ryanhepple@hotmail.com>
2022-05-15 11:10:08 -0700

    premium invoice start

--- src/app/main/apps/premiums/viewCart/ViewCart.js
+++ src/app/main/apps/premiums/viewCart/ViewCart.js
@@ -28,7 +28,7 @@ import { Card } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import clsx from "clsx";
 import { darken } from "@material-ui/core/styles/colorManipulator";
-
+import moment from "moment";
 const useStyles = makeStyles((theme) => ({
   root: {
     background: `radial-gradient(${darken(
@@ -52,6 +52,8 @@ function ViewCart(props) {
   const user = useSelector(({ auth }) => auth.user);
   const classes = useStyles();
 
+  const today = moment().format("YYYY-MM-DD");
+
   useEffect(() => {
     dispatch(getPremiumItems());
   }, [dispatch]);
@@ -72,6 +74,7 @@ function ViewCart(props) {
   }
 
   const invoice = {
+    number: Math.floor(Math.random() * 90000) + 10000,
     from: {
       name: "Lordco Auto Parts",
       address: "1525 Kingsway Ave, Port Coquitlam, BC V3C 1S2",
@@ -169,245 +172,243 @@ function ViewCart(props) {
             animate={{ opacity: 1, y: 0 }}
             transition={{ bounceDamping: 0 }}
           >
-            <div className="mx-auto w-xl print:w-full print:shadow-none rounded-none sm:rounded-20">
-              <div className="p-88 print:p-0">
-                <div className="flex flex-row justify-between items-start">
-                  <div className="flex flex-col">
-                    <div className="flex items-center mb-80 print:mb-0">
-                      <img
-                        className="w-160 print:w-60"
-                        src="assets/images/logos/lordco-logo.png"
-                        alt="logo"
-                      />
+            <div className="p-88 print:p-0">
+              <div className="flex flex-row justify-between items-start">
+                <div className="flex flex-col">
+                  <div className="flex items-center mb-80 print:mb-0">
+                    <img
+                      className="w-160 print:w-60"
+                      src="assets/images/logos/lordco-logo.png"
+                      alt="logo"
+                    />
 
-                      <div
-                        className={clsx(
-                          classes.divider,
-                          "mx-48 w-px h-128 print:mx-16"
-                        )}
-                      />
+                    <div
+                      className={clsx(
+                        classes.divider,
+                        "mx-48 w-px h-128 print:mx-16"
+                      )}
+                    />
 
-                      <div className="max-w-160">
+                    <div className="max-w-160">
+                      <Typography color="textSecondary">
+                        {invoice.from.name}
+                      </Typography>
+
+                      {invoice.from.address && (
                         <Typography color="textSecondary">
-                          {invoice.from.name}
+                          {invoice.from.address}
                         </Typography>
+                      )}
+                      {invoice.from.phone && (
+                        <Typography color="textSecondary">
+                          <span>Phone:</span>
+                          {invoice.from.phone}
+                        </Typography>
+                      )}
+                      {invoice.from.email && (
+                        <Typography color="textSecondary">
+                          <span>Email:</span>
+                          {invoice.from.email}
+                        </Typography>
+                      )}
+                    </div>
+                  </div>
 
-                        {invoice.from.address && (
-                          <Typography color="textSecondary">
-                            {invoice.from.address}
-                          </Typography>
-                        )}
-                        {invoice.from.phone && (
-                          <Typography color="textSecondary">
-                            <span>Phone:</span>
-                            {invoice.from.phone}
-                          </Typography>
-                        )}
-                        {invoice.from.email && (
-                          <Typography color="textSecondary">
-                            <span>Email:</span>
-                            {invoice.from.email}
-                          </Typography>
-                        )}
-                      </div>
+                  <div className="flex items-center">
+                    <div className="flex justify-end items-center w-160 print:w-60">
+                      <Typography
+                        variant="h5"
+                        className="font-light print:text-16"
+                        color="textSecondary"
+                      >
+                        SHIP TO
+                      </Typography>
                     </div>
 
-                    <div className="flex items-center">
-                      <div className="flex justify-end items-center w-160 print:w-60">
+                    <div
+                      className={clsx(
+                        classes.divider,
+                        "mx-48 w-px h-128 print:mx-16"
+                      )}
+                    />
+
+                    <div className="max-w-160">
+                      <Typography color="textSecondary">
+                        {user.data.companyName}
+                      </Typography>
+
+                      {user.data.address1 && (
+                        <Typography color="textSecondary">
+                          {user.data.address1} {user.data.address2}
+                        </Typography>
+                      )}
+                      {user.data.phoneNumber && (
+                        <Typography color="textSecondary">
+                          <span>Phone:</span>
+                          {user.data.phoneNumber}
+                        </Typography>
+                      )}
+                      {user.data.email && (
+                        <Typography color="textSecondary">
+                          <span>Email:</span>
+                          {user.data.email}
+                        </Typography>
+                      )}
+                    </div>
+                  </div>
+                </div>
+
+                <table>
+                  <tbody>
+                    <tr>
+                      <td className="pb-32">
                         <Typography
-                          variant="h5"
-                          className="font-light print:text-16"
+                          className="font-light"
+                          variant="h4"
                           color="textSecondary"
                         >
-                          SHIP TO
+                          ORDER#
                         </Typography>
-                      </div>
+                      </td>
+                      <td className="pb-32 px-16">
+                        <Typography className="font-light" variant="h4">
+                          {invoice.number}
+                        </Typography>
+                      </td>
+                    </tr>
 
-                      <div
-                        className={clsx(
-                          classes.divider,
-                          "mx-48 w-px h-128 print:mx-16"
-                        )}
-                      />
+                    <tr>
+                      <td className="text-right">
+                        <Typography color="textSecondary">DATE</Typography>
+                      </td>
+                      <td className="px-16">
+                        <Typography>{today}</Typography>
+                      </td>
+                    </tr>
 
-                      <div className="max-w-160">
+                    <tr>
+                      <td className="text-right">
                         <Typography color="textSecondary">
-                          {user.data.companyName}
+                          TOTAL REDEEMED
                         </Typography>
+                      </td>
+                      <td className="px-16">
+                        <Typography>$36,000</Typography>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
 
-                        {user.data.address1 && (
-                          <Typography color="textSecondary">
-                            {user.data.address1} {user.data.address2}
-                          </Typography>
-                        )}
-                        {user.data.phoneNumber && (
-                          <Typography color="textSecondary">
-                            <span>Phone:</span>
-                            {user.data.phoneNumber}
-                          </Typography>
-                        )}
-                        {user.data.email && (
-                          <Typography color="textSecondary">
-                            <span>Email:</span>
-                            {user.data.email}
-                          </Typography>
-                        )}
-                      </div>
-                    </div>
+              <div className="mt-96 print:mt-0">
+                <Table className="simple">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>SERVICE</TableCell>
+                      <TableCell>UNIT</TableCell>
+                      <TableCell align="right">UNIT PRICE</TableCell>
+                      <TableCell align="right">QUANTITY</TableCell>
+                      <TableCell align="right">TOTAL</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    <TableRow key={service.id}>
+                      <TableCell>
+                        <Typography className="mb-8" variant="subtitle1">
+                          {service.title}
+                        </Typography>
+                        <Typography variant="caption" color="textSecondary">
+                          {service.detail}
+                        </Typography>
+                      </TableCell>
+                      <TableCell>{service.unit}</TableCell>
+                      <TableCell align="right"></TableCell>
+                      <TableCell align="right">{service.quantity}</TableCell>
+                      <TableCell align="right"></TableCell>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+
+                <Table className="simple">
+                  <TableBody>
+                    <TableRow>
+                      <TableCell>
+                        <Typography
+                          className="font-normal"
+                          variant="subtitle1"
+                          color="textSecondary"
+                        >
+                          SUBTOTAL
+                        </Typography>
+                      </TableCell>
+                      <TableCell align="right">
+                        <Typography
+                          className="font-normal"
+                          variant="subtitle1"
+                          color="textSecondary"
+                        ></Typography>
+                      </TableCell>
+                    </TableRow>
+
+                    <TableRow>
+                      <TableCell>
+                        <Typography
+                          className="font-light"
+                          variant="h4"
+                          color="textSecondary"
+                        >
+                          TOTAL
+                        </Typography>
+                      </TableCell>
+                      <TableCell align="right">
+                        <Typography
+                          className="font-light"
+                          variant="h4"
+                          color="textSecondary"
+                        ></Typography>
+                      </TableCell>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+              </div>
+
+              <div className="mt-96 print:mt-0 print:px-16">
+                <div className="flex">
+                  <div className="flex-shrink-0">
+                    <img
+                      className="w-160 print:w-60"
+                      src="assets/images/logos/CustomerPortal.png"
+                      alt="logo"
+                    />
                   </div>
-
-                  <table>
-                    <tbody>
-                      <tr>
-                        <td className="pb-32">
-                          <Typography
-                            className="font-light"
-                            variant="h4"
-                            color="textSecondary"
-                          >
-                            RECEIPT
-                          </Typography>
-                        </td>
-                        <td className="pb-32 px-16">
-                          <Typography className="font-light" variant="h4">
-                            {invoice.number}
-                          </Typography>
-                        </td>
-                      </tr>
-
-                      <tr>
-                        <td className="text-right">
-                          <Typography color="textSecondary">DATE</Typography>
-                        </td>
-                        <td className="px-16">
-                          <Typography>{invoice.date}</Typography>
-                        </td>
-                      </tr>
-
-                      <tr>
-                        <td className="text-right">
-                          <Typography color="textSecondary">
-                            TOTAL REDEEMED
-                          </Typography>
-                        </td>
-                        <td className="px-16">
-                          <Typography></Typography>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
                 </div>
+                <Typography
+                  className="font-large mb-24"
+                  variant="h5"
+                  color="textSecondary"
+                >
+                  Thank-you for your business
+                </Typography>
+                <Typography variant="subtitle1" component="div">
+                  Comments
+                </Typography>
+                <TextField
+                  fullWidth
+                  placeholder="Comments..."
+                  multiline
+                  minRows={2}
+                  maxRows={4}
+                />
 
-                <div className="mt-96 print:mt-0">
-                  <Table className="simple">
-                    <TableHead>
-                      <TableRow>
-                        <TableCell>SERVICE</TableCell>
-                        <TableCell>UNIT</TableCell>
-                        <TableCell align="right">UNIT PRICE</TableCell>
-                        <TableCell align="right">QUANTITY</TableCell>
-                        <TableCell align="right">TOTAL</TableCell>
-                      </TableRow>
-                    </TableHead>
-                    <TableBody>
-                      <TableRow key={service.id}>
-                        <TableCell>
-                          <Typography className="mb-8" variant="subtitle1">
-                            {service.title}
-                          </Typography>
-                          <Typography variant="caption" color="textSecondary">
-                            {service.detail}
-                          </Typography>
-                        </TableCell>
-                        <TableCell>{service.unit}</TableCell>
-                        <TableCell align="right"></TableCell>
-                        <TableCell align="right">{service.quantity}</TableCell>
-                        <TableCell align="right"></TableCell>
-                      </TableRow>
-                    </TableBody>
-                  </Table>
-
-                  <Table className="simple">
-                    <TableBody>
-                      <TableRow>
-                        <TableCell>
-                          <Typography
-                            className="font-normal"
-                            variant="subtitle1"
-                            color="textSecondary"
-                          >
-                            SUBTOTAL
-                          </Typography>
-                        </TableCell>
-                        <TableCell align="right">
-                          <Typography
-                            className="font-normal"
-                            variant="subtitle1"
-                            color="textSecondary"
-                          ></Typography>
-                        </TableCell>
-                      </TableRow>
-
-                      <TableRow>
-                        <TableCell>
-                          <Typography
-                            className="font-light"
-                            variant="h4"
-                            color="textSecondary"
-                          >
-                            TOTAL
-                          </Typography>
-                        </TableCell>
-                        <TableCell align="right">
-                          <Typography
-                            className="font-light"
-                            variant="h4"
-                            color="textSecondary"
-                          ></Typography>
-                        </TableCell>
-                      </TableRow>
-                    </TableBody>
-                  </Table>
-                </div>
-
-                <div className="mt-96 print:mt-0 print:px-16">
-                  <div className="flex">
-                    <div className="flex-shrink-0">
-                      <img
-                        className="w-160 print:w-60"
-                        src="assets/images/logos/CustomerPortal.png"
-                        alt="logo"
-                      />
-                    </div>
-                  </div>
-                  <Typography
-                    className="font-large mb-24"
-                    variant="h5"
-                    color="textSecondary"
-                  >
-                    Thank-you for your business
-                  </Typography>
-                  <Typography variant="subtitle1" component="div">
-                    Comments
-                  </Typography>
-                  <TextField
+                <div className="mt-24">
+                  <Button
+                    className="whitespace-nowrap"
+                    variant="contained"
                     fullWidth
-                    placeholder="Comments..."
-                    multiline
-                    minRows={2}
-                    maxRows={4}
-                  />
-
-                  <div className="mt-24">
-                    <Button
-                      className="whitespace-nowrap"
-                      variant="contained"
-                      fullWidth
-                      color="primary"
-                    >
-                      Place Order
-                    </Button>
-                  </div>
+                    color="primary"
+                  >
+                    Place Order
+                  </Button>
                 </div>
               </div>
             </div>
