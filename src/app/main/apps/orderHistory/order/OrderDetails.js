d68f60c07f781edecb891e2cc77bb471066fac0f
Ryan <ryanhepple@hotmail.com>
2022-05-19 18:51:24 -0700

    updates

--- src/app/main/apps/orderHistory/order/OrderDetails.js
+++ src/app/main/apps/orderHistory/order/OrderDetails.js
@@ -5,7 +5,7 @@ import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import { Typography } from "@material-ui/core";
 import { useSelector } from "react-redux";
-
+import { motion } from "framer-motion";
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#fff" : "#fff",
   ...theme.typography.body2,
@@ -16,80 +16,92 @@ const Item = styled(Paper)(({ theme }) => ({
 function OrderDetails(props) {
   const user = useSelector(({ auth }) => auth.user);
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={0}>
-        <Grid
-          style={{
-            backgroundColor: "#777",
-            padding: "25px",
-          }}
-          item
-          xs={12}
-        >
-          <img
-            src={
-              "https://customer.lordco.com/lordcoImages/lordco-logo-dark.png"
-            }
-            style={{ width: "180px", height: "60px", float: "left" }}
-          />
-          <Typography
-            variant="h3"
-            className="font-semibold"
-            style={{ float: "right", color: "white" }}
+    <motion.div
+      className="w-full"
+      initial={{ opacity: 0, x: 20 }}
+      animate={{ opacity: 1, x: 0, transition: { delay: 0.2 } }}
+    >
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={0}>
+          <Grid
+            style={{
+              backgroundColor: "#777",
+              padding: "25px",
+            }}
+            item
+            xs={12}
           >
-            INVOICE
-          </Typography>
-        </Grid>
-        <Grid item xs={4} container>
-          <div className="mt-12 p-24">
-            <Typography className="text-13 font-semibold" color="textSecondary">
-              Bill To:
-            </Typography>
-            <Typography color="textSecondary">
-              {user.data.companyName}
-            </Typography>
-
-            {user.data.address1 && (
-              <Typography color="textSecondary">
-                {user.data.address1} {user.data.address2} {user.data.city}{" "}
-                {user.data.province}&nbsp;
-                {user.data.postalCode}
-              </Typography>
-            )}
-          </div>
-        </Grid>
-        <Grid item xs={4}>
-          <div className="mt-12 p-24">
-            <Typography className="text-13 font-semibold" color="textSecondary">
-              Ship To:
-            </Typography>
-            <Typography color="textSecondary">
-              {user.data.companyName}
-            </Typography>
-
-            {user.data.address1 && (
-              <Typography color="textSecondary">
-                {user.data.address1} {user.data.address2} {user.data.city}{" "}
-                {user.data.province}&nbsp;
-                {user.data.postalCode}
-              </Typography>
-            )}
-          </div>
-        </Grid>
-        <Grid item xs={4}>
-          <div className="mt-12 p-24">
+            <img
+              src={
+                "https://customer.lordco.com/lordcoImages/lordco-logo-dark.png"
+              }
+              style={{ width: "180px", height: "60px", float: "left" }}
+            />
             <Typography
-              variant="h5"
+              variant="h3"
               className="font-semibold"
-              color="textSecondary"
+              style={{ float: "right", color: "white" }}
             >
-              Invoice# {props.invoiceNumber}
+              INVOICE
             </Typography>
-            <Typography color="textSecondary">Date:</Typography>
-          </div>
+          </Grid>
+          <Grid item xs={4} container>
+            <div className="mt-12 p-24">
+              <Typography
+                className="text-13 font-semibold"
+                color="textSecondary"
+              >
+                Bill To:
+              </Typography>
+              <Typography color="textSecondary">
+                {user.data.companyName}
+              </Typography>
+
+              {user.data.address1 && (
+                <Typography color="textSecondary">
+                  {user.data.address1} {user.data.address2} {user.data.city}{" "}
+                  {user.data.province}&nbsp;
+                  {user.data.postalCode}
+                </Typography>
+              )}
+            </div>
+          </Grid>
+          <Grid item xs={4}>
+            <div className="mt-12 p-24">
+              <Typography
+                className="text-13 font-semibold"
+                color="textSecondary"
+              >
+                Ship To:
+              </Typography>
+              <Typography color="textSecondary">
+                {user.data.companyName}
+              </Typography>
+
+              {user.data.address1 && (
+                <Typography color="textSecondary">
+                  {user.data.address1} {user.data.address2} {user.data.city}{" "}
+                  {user.data.province}&nbsp;
+                  {user.data.postalCode}
+                </Typography>
+              )}
+            </div>
+          </Grid>
+          <Grid item xs={4}>
+            <div className="mt-12 p-24">
+              <Typography
+                variant="h5"
+                className="font-semibold"
+                color="textSecondary"
+              >
+                Invoice# {props.invoiceNumber}
+              </Typography>
+              <Typography color="textSecondary">Date:</Typography>
+            </div>
+          </Grid>
         </Grid>
-      </Grid>
-    </Box>
+      </Box>
+    </motion.div>
   );
 }
 
--- src/app/main/apps/orderHistory/order/OrderHeader.js
+++ src/app/main/apps/orderHistory/order/OrderHeader.js
@@ -2,42 +2,59 @@ import { Typography, Button, Icon, Input, Paper } from "@material-ui/core";
 import { ThemeProvider } from "@material-ui/core/styles";
 import { motion } from "framer-motion";
 import { useDispatch, useSelector } from "react-redux";
-import { selectMainTheme } from "app/store/fuse/settingsSlice";
+import { useTheme } from "@material-ui/core";
 //import { setPremiumItemsSearchText } from "../store/premiumItemsSlice";
+import { Link } from "react-router-dom";
 
 function OrderHeader(props) {
   //const dispatch = useDispatch();
   // const searchText = useSelector(
   //   ({ Premiums }) => Premiums.premiumItems.searchText
   // );
-  const mainTheme = useSelector(selectMainTheme);
 
+  const theme = useTheme();
   return (
     <div className="flex flex-1 w-full items-center justify-between">
-      <div className="flex items-center">
-        <Icon
-          component={motion.span}
-          initial={{ scale: 0 }}
-          animate={{ scale: 1, transition: { delay: 0.2 } }}
-          className="text-24 md:text-32"
+      <div className="flex flex-col items-start max-w-full min-w-0">
+        <motion.div
+          initial={{ x: 20, opacity: 0 }}
+          animate={{ x: 0, opacity: 1, transition: { delay: 0.3 } }}
         >
-          receipt
-        </Icon>
-        <Typography
-          component={motion.span}
-          initial={{ x: -20 }}
-          animate={{ x: 0, transition: { delay: 0.2 } }}
-          delay={300}
-          className="hidden sm:flex text-16 md:text-24 mx-12 font-semibold"
-        >
-          Invoice #{props.invoiceNumber}
-        </Typography>
+          <Typography
+            className="flex items-center sm:mb-12"
+            component={Link}
+            role="button"
+            to="/apps/premiums/products"
+            color="inherit"
+          >
+            <Icon className="text-20">
+              {theme.direction === "ltr" ? "arrow_back" : "arrow_forward"}
+            </Icon>
+            <span className="hidden sm:flex mx-4 font-medium">
+              Back to Orders
+            </span>
+          </Typography>
+          <div className="flex items-center max-w-full">
+            <Icon
+              component={motion.span}
+              initial={{ scale: 0 }}
+              animate={{ scale: 1, transition: { delay: 0.2 } }}
+              className="text-24 md:text-32"
+            >
+              receipt
+            </Icon>
+            <Typography
+              component={motion.span}
+              initial={{ x: -20 }}
+              animate={{ x: 0, transition: { delay: 0.2 } }}
+              delay={300}
+              className="hidden sm:flex text-16 md:text-24 mx-12 font-semibold"
+            >
+              Invoice #{props.invoiceNumber}
+            </Typography>
+          </div>
+        </motion.div>
       </div>
-
-      <motion.div
-        initial={{ opacity: 0, x: 20 }}
-        animate={{ opacity: 1, x: 0, transition: { delay: 0.2 } }}
-      ></motion.div>
     </div>
   );
 }
